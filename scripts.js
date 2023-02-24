4c1a65b6857ce76a545c39007526f75b038e0d1c
Lycan <lycanfsd@gmail.com>
2023-02-24 17:21:42 -0600

    Fix operate function, allow for decimals, sign change, and percent
    Operate function now takes operator and two variables instead of
    array. Fixed the calculation functions to reflex this change.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,97 @@
-function add() {
-    const numbers = [...arguments];
-    return numbers.reduce((total, arguments) => total + arguments);
-}
+const buttons = document.querySelectorAll('button');
+const displayCalc = document.querySelector('#display-text');
+const storedDisplayCalc = document.querySelector('#storedDisplayText');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('.clear');
+const signButton = document.querySelector('.signChange');
+const percentButton = document.querySelector('.percent');
+const equals = document.querySelector('.equal');
 
-function subtract() {
-    const numbers = [...arguments];
-    return numbers.reduce((total, arguments) => total - arguments);
-}
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
-function multiply() {
-    const numbers = [...arguments];
-    return numbers.reduce((total, arguments) => total * arguments);
+buttons.forEach(button => {button.addEventListener('click', () => processButton(button))});
 
-}
-
-function divide() {
-    const numbers = [...arguments];
-    return numbers.reduce((total, arguments) => total / arguments);
-}
-
-//takes an operator and array of numbers and then calls one of the above functions on the numbers
-function operate(operator, numArray) {
+function operate(operator, previousValue, currentValue) {
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
     switch (operator) {
         case '+':
-            return add(...numArray);
-        case '-':
-            return subtract(...numArray);
-        case '*':
-            return multiply(...numArray);
-        case '/':
-            return divide(...numArray);
+            previousValue+=currentValue;
+            break;
+        case '−':
+            previousValue-=currentValue;
+            break;
+        case 'x':
+            previousValue*=currentValue;
+            break;
+        case '÷':
+            if (currentValue == 0) {
+                return "Cannot divide by 0";
+            }
+            previousValue/=currentValue;
+            break;
+    }
+    previousValue = roundNum(previousValue);
+    return previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+function processButton(button) {
+    if (displayCalc.textContent == "Cannot divide by 0") {
+        clearCalc();
+        displayCalc.textContent = '0';
+        storedDisplayCalc.textContent = '';
+    } else {
+        if (button.classList.contains('operator')) {
+            processOperator(button.textContent);
+            storedDisplayCalc.textContent = `${previousValue} ${operator}`;
+            displayCalc.textContent = currentValue;
+        } else if (button.classList.contains('number')) {
+            currentValue += button.textContent;
+            displayCalc.textContent = currentValue;
+        } else if (button.classList.contains('clear')) {
+            clearCalc();
+        } else if (button.classList.contains('equal')) {
+            if (currentValue != '' && previousValue != '') {
+                storedDisplayCalc.textContent = '';
+                displayCalc.textContent = operate(operator, previousValue, currentValue);
+                currentValue = displayCalc.textContent;
+            }
+        } else if (button.classList.contains('decimal')) {
+            processDecimal();
+        } else if (button.classList.contains('percent')) {
+            currentValue /= 100;
+            displayCalc.textContent = currentValue;
+        } else if (button.classList.contains('signChange')) {
+            currentValue *= -1;
+            displayCalc.textContent = currentValue;
+        }
     }
 }
 
-//functions that populate the display
+function processOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
+
+function processDecimal() {
+    if (!currentValue.includes('.')) {
+        currentValue += '.';
+    }
+}
+
+function clearCalc() {
+    previousValue = '';
+    currentValue = '';
+    operator = '';
+    displayCalc.textContent = '0';
+    storedDisplayCalc.textContent = '';
+}
+
+function roundNum(number) {
+    return Math.round(number * 1000) / 1000;
+}
